724db6a680b5d8a287b833861a3662105b2d6d34
royal.bk <royal.bk@essteyr.com>
2020-05-27 18:30:37 +0530

    Test cases added with Red green and refactor method

--- T1_FizzBuzz/T1_FizzBuzz/T1_FizzBuzz.cpp
+++ T1_FizzBuzz/T1_FizzBuzz/T1_FizzBuzz.cpp
@@ -9,9 +9,21 @@
 
 std::string FizzBuzz(int value)
 {
+    if (value%3 == 0)
+        return "Fizz";
+    if (value%5 == 0)
+        return "Buzz";
     return std::to_string(value);
 }
 
+
+// Make a Utility function that can call all similar casee
+
+void CheckFizzBuzz(int value, std::string name)
+{
+    std::string result = FizzBuzz(value);
+    ASSERT_STREQ(name.c_str(), result.c_str());
+}
 
 
 //Case 1
@@ -46,4 +58,55 @@ TEST(MyTestExample, Return2With2PassedIn)
     ASSERT_STREQ("2", result.c_str());
 }
 
+
+//Replace the previous codes with new utility
+TEST(MyTestExample, Return1With1PassedInNew)
+{
+    CheckFizzBuzz(1, "1");
+}
+
+//Replace the previous codes with new utility
+TEST(MyTestExample, Return2With2PassedInNew)
+{
+    CheckFizzBuzz(2, "2");
+}
+
+
+//Case 4
+// RED PHASE - FAILING UNIT TEST
+//GREEN PHASE - MAke it kust work
+// REFACTOR PHASE . 
+TEST(MyTestExample, ReturnFizzWith3PassedIn)
+{
+    CheckFizzBuzz(3,"Fizz");
+}
+
+
+//Case 5
+// RED PHASE - FAILING UNIT TEST
+//GREEN PHASE - MAke it kust work
+// REFACTOR PHASE . 
+TEST(MyTestExample, ReturnBuzzWith5PassedIn)
+{
+    CheckFizzBuzz(5, "Buzz");
+}
+
+//Case 6
+// RED PHASE - FAILING UNIT TEST
+//GREEN PHASE - MAke it kust work
+// REFACTOR PHASE . 
+TEST(MyTestExample, ReturnFizzWith6PassedIn)
+{
+    CheckFizzBuzz(6, "Fizz");
+}
+
+
+//Case 7
+// RED PHASE - FAILING UNIT TEST
+//GREEN PHASE - MAke it kust work
+// REFACTOR PHASE . 
+TEST(MyTestExample, ReturnBuzzWith10PassedIn)
+{
+    CheckFizzBuzz(10, "Buzz");
+}
 
